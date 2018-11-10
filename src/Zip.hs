d2cd67ea5a826e153190fffd6a769d9e2d5a9f7f
Felix Scheinost <felix.scheinost@posteo.de>
2018-11-10 15:01:04 +0100

    Unzip with progress

--- src/Zip.hs
+++ src/Zip.hs
@@ -1,28 +1,61 @@
 module Zip (
-    getFiles,
-    getSingleFile
+    getFilePathsInZip,
+    saveEntry,
+    unpackInto,
+    UnzipProgress
 ) where
 
-import qualified Algorithms.NaturalSort as NS
-import qualified Codec.Archive.Zip      as Z
-import           Control.Monad.Catch    (throwM)
-import           Control.Monad.IO.Class (MonadIO)
-import           Data.ByteString        (ByteString)
-import           Data.Conduit           (ConduitT)
-import           Data.List              (sortBy)
-import qualified Data.Map               as M
-import           Import                 (Handler)
-import           Numeric.Natural
+import qualified Codec.Archive.Zip           as Zip
+import qualified Control.Concurrent.STM      as STM
+import           Control.Concurrent.STM.TVar
+import           Control.Monad
+import           Control.Monad.IO.Class      (MonadIO, liftIO)
+import qualified Data.ByteString             as BS
+import           Data.Conduit                ((.|))
+import qualified Data.Conduit                as Conduit
+import qualified Data.Conduit.Binary         as Conduit
+import qualified Data.Conduit.Combinators    as Conduit
+import qualified Data.List                   as List
+import qualified Data.Map                    as Map
+import qualified Data.Set                    as Set
+import           GHC.Exts                    (toList)
+import           System.Directory
+import           System.FilePath             ((</>))
+import qualified System.FilePath             as FP
 
-getFiles :: MonadIO m => FilePath -> m [FilePath]
-getFiles fp = Z.withArchive fp (processKeys <$> Z.getEntries)
-    where
-        processKeys = sortBy NS.compare . map Z.unEntrySelector . M.keys
+getFilePathsInZip :: MonadIO m => FilePath -> m [FilePath]
+getFilePathsInZip fp = Zip.withArchive fp (fmap Zip.unEntrySelector . Map.keys <$> Zip.getEntries)
 
-getSingleFile :: FilePath -> FilePath -> IO (ConduitT () ByteString Handler (), Natural)
-getSingleFile zipFp fileFp = Z.withArchive zipFp $ do
-    entry <- Z.mkEntrySelector fileFp
-    let entryNotFound = throwM (Z.EntryDoesNotExist zipFp entry)
-    entryDesc <- Z.getEntryDesc entry >>= maybe entryNotFound return
-    source <- Z.getEntrySource entry
-    return (source, Z.edUncompressedSize entryDesc)
+type UnzipProgress = (Integer, Integer)
+
+-- | A version of Zip.saveEntry that can monitor the progress of the operation
+saveEntry :: Zip.EntrySelector -> FilePath -> ((UnzipProgress -> UnzipProgress) -> IO ()) -> Zip.ZipArchive ()
+saveEntry s path updateProgress = do
+    let sink = Conduit.sinkFileCautious path
+    let updateProgressPipe = Conduit.iterM $ \bytes -> liftIO  $ updateProgress $
+            \(delta, total) -> (delta + fromIntegral (BS.length bytes), total)
+    source <- Zip.getEntrySource s
+    liftIO $ Conduit.runConduitRes (source .| updateProgressPipe .| sink)
+    med <- Zip.getEntryDesc s
+    forM_ med (liftIO . setModificationTime path . Zip.edModTime)
+
+-- | A version of Zip.unpackInto that can monitor the progress of the operation and returns the filepaths of the created files
+unpackInto :: FilePath -> FilePath -> ((Maybe UnzipProgress -> Maybe UnzipProgress) -> IO ()) -> IO [FilePath]
+unpackInto dir' zipPath updateProgress = Zip.withArchive zipPath $ do
+    selectors <- Map.keysSet <$> Zip.getEntries
+    if null selectors then
+        return []
+    else do
+        dir <- liftIO (makeAbsolute dir')
+        liftIO (createDirectoryIfMissing True dir)
+        let dirs =
+                Set.map
+                    (FP.takeDirectory . (dir </>) . Zip.unEntrySelector)
+                    selectors
+        forM_ dirs (liftIO . createDirectoryIfMissing True)
+        let entrySize s = maybe 0 Zip.edUncompressedSize <$> Zip.getEntryDesc s
+        totalSize <- foldM (\size sel -> (size +) <$> entrySize sel) 0 selectors
+        liftIO $ updateProgress $ const $ Just (fromIntegral totalSize, 0)
+        forM (toList selectors) $ \s -> do
+            saveEntry s (dir </> Zip.unEntrySelector s) (updateProgress . fmap)
+            return $ dir </> Zip.unEntrySelector s
